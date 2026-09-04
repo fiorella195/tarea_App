14407bb29afd754fe039cc3c4cf4e3364adf3f96
fiorella195 <fiorella15082007@gmail.com>
2026-09-04 09:12:02 -0600

    Creación de Data Class y DatabaseHelper

---
+++ app/src/main/java/com/example/notasapp_sqlite/Nota.kt
@@ -0,0 +1,4 @@
+package com.example.notasapp_sqlite
+
+data class Nota (val id : Int, val titulo : String, val descripcion : String)
+
---
+++ app/src/main/java/com/example/notasapp_sqlite/NotasDatasBaseHelp.kt
@@ -0,0 +1,36 @@
+package com.example.notasapp_sqlite
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class NotasDatasBaseHelp (context: Context) : SQLiteOpenHelper(
+    context,DATABASE_NAME, null, DATABASE_VERSION
+){
+    override fun onCreate(p0: SQLiteDatabase?) {
+        val createTableQuery =
+            "CREATE TABLE $TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY, $COLUMNS_TITLE TEXT, $COLUMN_DESCRIPTION TEXT)"
+        p0?.execSQL(createTableQuery)
+    }
+
+    override fun onUpgrade(
+        p0: SQLiteDatabase?,
+        p1: Int,
+        p2: Int
+    ) {
+       val dropTableQuery =
+           "DROP TABLE IF EXISTS $TABLE_NAME"
+        p0?.execSQL(dropTableQuery)
+        onCreate(p0)
+        
+    }
+
+    companion object{
+        private const val DATABASE_NAME ="Notas.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "notas"
+        private const val COLUMN_ID = "id"
+        private const val COLUMNS_TITLE = "titulo"
+        private const val COLUMN_DESCRIPTION = "descripcion"
+    }
+}
